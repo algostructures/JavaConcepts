889de0a6b6731128119f4b6deca06b63655a5793
saurabh <saurabh945644@gmail.com>
2018-08-21 23:23:25 +0530

    Initial commit

---
+++ src/main/java/com/example/javaconcepts/JavaconceptsApplication.java
@@ -0,0 +1,12 @@
+package com.example.javaconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaconceptsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaconceptsApplication.class, args);
+    }
+}
